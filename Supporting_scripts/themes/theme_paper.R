e140bbc899b2d252ac73a1aff4fc9fb8960d7f48
nikita-a-v <nikitaverheyden@aol.com>
2023-05-17 17:26:42 +0200

    Theme changes
    Ich habe noch ein bisschen im theme rumgepfuscht

--- Supporting_scripts/themes/theme_paper.R
+++ Supporting_scripts/themes/theme_paper.R
@@ -10,9 +10,9 @@ theme_paper <- function(){
       #grid elements
       panel.grid.major = element_blank(),  #strip major gridlines
       panel.grid.minor = element_blank(),    #strip minor gridlines       #strip axis ticks
-      plot.background = element_blank(),
+      plot.background = element_rect(fill='transparent', color=NA), #transparent plot bg
       #panel.border=element_rect(color="black", fill = NA),
-      panel.background = element_blank(),
+      panel.background = element_rect(fill='transparent', color = "transparent"), #transparent panel bg
       
       #axis lines
       axis.line = element_line(colour = 'black', size = 0.5),
@@ -22,14 +22,19 @@ theme_paper <- function(){
       
       #text elements
       
+      plot.title = element_text(
+        size = 8,
+        color = "black"),
+      
+      
       axis.title = element_text(             #axis titles
         #family = font,            #font family
         size = 8,
         color = "black"),               #font size
       
       axis.text = element_text(              #axis text
-        #family = font,            #axis famuly
-        size = 7,
+        #family = font,            #axis family
+        size = 6,
         color = "black"),                #font size
       
       axis.text.x = element_text(            #margin for axis text
@@ -37,13 +42,15 @@ theme_paper <- function(){
         color = "black"),
       
       legend.text = element_text(              #axis text
-        #family = font,            #axis famuly
-        size = 7,
+        #family = font,            #axis family
+        size = 8,
         color = "black"),  
 
       
       legend.title = element_blank(),
-      legend.background = element_blank()
+      legend.background = element_rect(fill='transparent', color = "transparent"), #transparent legend bg
+      legend.box.background = element_rect(fill='transparent', color = "transparent"), #transparent legend panel
+      legend.position = c(0.8, 0.35) # positions the legend into the plot (can be moved later in affinity designer, but makes setting the size easier)
 
       
       #since the legend often requires manual tweaking 
